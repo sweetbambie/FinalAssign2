2ddf7962017db0384920c1d91c1e7789553948a0
sweetbambie <sophia@miao.fm>
2024-12-02 23:04:35 -0500

    Update Feature.vue

--- src/components/Feature.vue
+++ src/components/Feature.vue
@@ -23,16 +23,44 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="Featured Movies">
-    <div v-if="response" class="movie-card">
-      <div v-for="number in numbers" :key="response.data.results[number].id" class="movie-card">
-        <img :src="`https://image.tmdb.org/t/p/w500${response.data.results[number].poster_path}`" alt="Movie Poster" class="movie-poster" />
-        <p class="movie-title">{{ response.data.results[number].title }}</p>
-      </div>
+<div class="text">Featured Movies</div>
+  <div v-if="response" class="movie-card">
+    <div v-for="number in numbers" :key="response.data.results[number].id" class="movie-card">
+      <img :src="`https://image.tmdb.org/t/p/w500${response.data.results[number].poster_path}`" alt="Movie Poster" class="movie-poster" />
+      <p class="movie-title">{{ response.data.results[number].title }}</p>
     </div>
   </div>
 </template>
-  
-<style scoped> 
- 
+
+<style scoped>
+.movie-card {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  gap: 2%;
+  margin-top: 4%;
+  margin-bottom: 8%;
+  align-items: center;
+  position:relative;
+}
+
+.FeaturedMovies {
+  display: flex;
+  gap: 2%;
+  flex-direction: row;   
+}
+
+.movie-card img {
+  flex: 1;
+  width: 22.5%;
+  height: auto;
+  object-fit: cover;
+}
+
+.movie-title {
+  font-size: 1.1rem;
+  margin-top: 10px;
+  color: hotpink;
+  vertical-align: middle;
+}
 </style>
